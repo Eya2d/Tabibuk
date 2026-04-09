ab3fb65a93ad0ff4044df29bc442b5e674918f21
Eyad <eyadscript@gmail.com>
2026-04-09 16:29:16 +0300

    Update app.js

--- Script/app.js
+++ Script/app.js
@@ -1,18 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  // منع الكليك يمين وسحب للروابط
   document.querySelectorAll("a").forEach(link => {
-    link.addEventListener("dragstart", e => e.preventDefault());
-    link.addEventListener("contextmenu", e => e.preventDefault());
-    link.addEventListener("mousedown", e => {
-      if (e.button === 0) e.preventDefault(); // زر الماوس الأيسر
-    });
-  });
 
-  // منع الكليك يمين وسحب للصور
-  document.querySelectorAll("img").forEach(img => {
-    img.addEventListener("contextmenu", e => e.preventDefault());
-    img.addEventListener("dragstart", e => e.preventDefault());
+    link.addEventListener("dragstart", e => e.preventDefault());
+
+    link.addEventListener("contextmenu", e => e.preventDefault());
+
+    link.addEventListener("mousedown", e => {
+      if (e.button === 0) e.preventDefault(); // زر الماوس
+    });
+
   });
 
 });
